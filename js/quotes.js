6d385fdfd8e567b80a375ac40f0ee3548975e792
Tim E. Sneddon <tim@sneddon.id.au>
2013-07-19 16:00:48 +0800

    Beginnings of an automatic quote generator.  Want to include these under the page titles everywhere, but the main page.  Some pseudo code also and the quotes picked out of the ZK messages file.

---
+++ js/quotes.js
@@ -0,0 +1,63 @@
+var quotes = new Array();
+
+load up the array
+
+var q = quotes.length();
+var i = Math.round(Math.random()*(q-1));
+return quotes[i];
+
+message ball_explodes
+<The glowing crystal ball begins to shine very brightly and a great deal>
+<of heat is dissipated until finally the crystal ball explodes violently,>
+<killing you in the process.>
+
+message cabinet
+<As you touch the cabinet, the lights in the building flicker and you hear>
+<a loud agonizing scream. The implementors, returning though a hitherto unknown>
+<passage, are appalled at your bravado. (It seems that an ACL was set on the>
+<cabinets). Having no pity, they quickly destroy you by executing ZAP and then>
+<proceed to slice you into little pieces.>
+
+message coke_cant_close
+<You can't close a can of Coke once it's been opened!>
+
+message coke_drinking
+<Glug...Glug...Glug...Glug...Ahhhhhhhhh.  That really hit the spot!>
+
+essage console_message
+<You hear something printed on the console.>
+
+message collapse
+<Talking to yourself is said to be a sign of impending mental collapse.>
+
+message dark_enter
+<It's now pitch black.  Fortunately there are no grues in ZK.  You'd>
+<better be careful, though, or you might walk into a wall and knock>
+<yourself unconscious.>
+
+message died_wall
+<Just like a developer! You walked into a wall and knocked yourself>
+<unconscious.>
+
+message having_fun
+<Having fun?>
+
+message guard_passes_through
+<One of the building's security guards comes up to you.  Seeing that you're>
+<wearing a temporary badge he bids you a good evening and moves on.>
+
+message orange_button
+<There is the slightest sensation of movement, but the elevator does not>
+<come to rest. It seems to be caught in an infinite loop searching for the>
+<orange floor. After a prolonged period of time, you have used up all the>
+<air in the elevator and die of suffocation.>
+
+message player_died
+<"He's dead, Jim.">
+
+<You really shouldn't be playing during prime time, but we'll make an>
+<exception just this once...>
+
+<Arg! Ack! Oop!>
+
+
